a601b27adff6a614e46e6e0fe0a5e90b83c8c398
h4ndzdatm0ld <hugotinoco@icloud.com>
2021-05-04 21:19:30 -0700

    stuff

--- mpls_in_the_sdn_era/mpls_sdn_era_batfish/bf_mpls_sdn.py
+++ mpls_in_the_sdn_era/mpls_sdn_era_batfish/bf_mpls_sdn.py
@@ -1,3 +1,4 @@
+"""Interactive Batfish Script."""
 from pybatfish.client.commands import bf_upload_diagnostics
 from pybatfish.question import bfq
 from pybatfish.client.asserts import (
@@ -7,6 +8,39 @@ from pybatfish.client.asserts import (
     assert_no_forwarding_loops,
     assert_no_undefined_references,
 )
+from pybatfish.question import load_questions
+from pybatfish.client.commands import (
+    bf_init_snapshot,
+    bf_set_network,
+    bf_session,
+    bf_set_snapshot,
+)
 
-# TODO: Make sure to simply run pytest and generate the latest configs using Nornir
-CONFIGS = "../tests/network_data/mpls_sdn_era/ASN65000/configs"
+# CONFIGS = "../../tests/network_data/mpls_sdn_era"
+CONFIGS = "../../mpls_in_the_sdn_era/mpls_sdn_era_nornir/napalm_getters/ASN65000/"
+
+
+def snapshot_loader(snap_path, name, overwrite=True):
+    """Simple function to load a snapshot into Batfish.
+
+    This function allows up to use a setup fixture and extend to multiple
+    test cases against thedifferent snapshots of the network.
+    """
+    bf_init_snapshot(snap_path, name=name, overwrite=overwrite)
+
+
+def main():
+    """Execute Batfish Interactive."""
+    bf_set_network("mpls_sdn_era")
+    # Load Questions
+    load_questions()
+
+    snapshot_loader(CONFIGS, "MPLS_SDN_ERA")
+    bf_set_snapshot("MPLS_SDN_ERA")
+
+    config_process = bfq.bgpProcessConfiguration().answer().frame()
+    refs = bfq.unusedStructures().answer().frame()
+    parse = bfq.fileParseStatus().answer().frame()
+
+
+main()
