dbb21250ce4ff7c80afed6477e3463496fcec21c
h4ndzdatm0ld <hugotinoco@icloud.com>
2023-01-15 05:22:11 +0000

    Reworked conftest/batfish tests

---
+++ clab-spauto-mpls-sdn-core.yml/ceos2/flash/if-wait.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+INTFS=$(echo $CLAB_INTFS)
+SLEEP=0
+
+int_calc () 
+{
+    index=0
+    for i in $(ls -1v /sys/class/net/ | grep 'eth\|ens\|eno\|^e[0-9]'); do
+      let index=index+1
+    done
+    MYINT=$index
+}
+
+int_calc
+
+echo "Waiting for all $INTFS interfaces to be connected"
+while [ "$MYINT" -lt "$INTFS" ]; do
+  echo "Connected $MYINT interfaces out of $INTFS"
+  sleep 1
+  int_calc
+done
+
+echo "Sleeping $SLEEP seconds before boot"
+sleep $SLEEP
+
