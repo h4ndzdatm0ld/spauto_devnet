eb32fa3bc0e540f6767e96fd486d848c05577ab7
Hugo Tinoco <43675476+h4ndzdatm0ld@users.noreply.github.com>
2023-01-15 23:35:32 -0700

    Links (#29)
    * Interfaces
    
    * Normalized, Batfish tests/assertions all passing on containerlab

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
+from typing import List
+
 import pytest
 from nornir import InitNornir
 from nornir_jinja2.plugins.tasks import template_file
 from nornir_utils.plugins.tasks.data import load_yaml
 from nornir_utils.plugins.tasks.files import write_file
 from pybatfish.client.commands import bf_init_snapshot
-from pybatfish.question import load_questions, bfq
 from pybatfish.client.session import Session
-from typing import List
+from pybatfish.question import bfq, load_questions
 
-NORNIR_PATH: str = "spauto/spauto_nornir"
+NORNIR_PATH: str = "spauto/nornir"
 BATFISH_HOST: str = "localhost"
 SNAPSHOT_PATH: str = "tests/network_data/mpls_sdn_era"
 CONFIGS_DIR: str = f"{SNAPSHOT_PATH}/configs/"
@@ -42,7 +43,7 @@ def devices():
     return network_devices
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="session")
 def batfish_session():
     batfish = Session(host=BATFISH_HOST)
     batfish.set_network(NETWORK_NAME)
@@ -51,6 +52,7 @@ def batfish_session():
     return batfish
 
 
+@pytest.mark.usefixtures("batfish_session")
 @pytest.fixture
 def bgp_config():
     """Use the pybatfish SDK to extract Panda Data frame answer
@@ -93,61 +95,6 @@ def load_data(task):
         file=f"{NORNIR_PATH}/data_input/{task.host.platform}/{task.host}.yml",
     )
     task.host["data_input"] = data.result
-
-
-# # def generate_full_mesh_list(task):
-# #     """Loop through inventory hosts which are MPLS enabled devices.
-
-# #     Generate a list of Loopback IP far-end addresses and add to a task_host[dict]
-# #     to reference later as we deploy our full-mesh MPLS LSP Configuration.
-
-# #     This task 100% depends on the success of "load_all_data" task.
-# #     """
-# #     # Create a list of devices from our inventory which are MPLS enabled.
-# #     # List comprehension will be ran on each host to gather a list and access the
-# #     # host loopback IP, which will be used as the far-end IP for the MPLS LSP.
-# #     FULL_MESH_DEVICES = [
-# #         host
-# #         for host in nornir.inventory.hosts.keys()
-# #         if "AS65000" in host and "RR" not in host and "P1" not in host
-# #         if "P2" not in host
-# #     ]
-# #     # Remove the current task host out of the list. We don't need our local device in this
-# #     # List as our goal is to gather data from the rest of the inventory from each device.
-# #     # We wrap in a try/block as we are popping items we KNOW are not in that list (CE devices)
-# #     # from our previous filtering.
-# #     try:
-# #         FULL_MESH_DEVICES.remove(f"{task.host}")
-# #     except ValueError:
-# #         pass
-
-# #     # The only reason we are able to access some of these external data values is because
-# #     # we loaded the external data in a previous task and added to the task.host['data_input] dict.
-# #     # We use some more list comprehension to extract all the interfaces from each host.
-# #     all_hosts_interfaces = [
-# #         nornir.inventory.hosts[device]["data_input"]["ip_interfaces"]
-# #         for device in FULL_MESH_DEVICES
-# #     ]
-
-# #     # Combine lists of lists into one list. We compile ALL interfaces from ALL devices besides
-# #     # The current task host. We need this so we can now create one final list of all the actual
-# #     # IP Addresses from the Loopbacks.
-# #     interfaces = list(itertools.chain.from_iterable(all_hosts_interfaces))
-
-# #     # Finally! Create a list of all the far-end system IP addresses (Loopbacks) to use as our
-# #     # Far-END destination for configuring our full-mesh of MPLS LSPs from the current task.host.
-# #     loopbacks = [
-# #         ip.get("ip_address")
-# #         for ip in interfaces
-# #         if ip.get("description") == "SYSTEM_LO_IP"
-# #     ]
-
-#     # Check the task.host vars and ensure ifull_mesh key is True under the MPLS dict
-#     # Take the newly created list and add it to a task.host[dictionary] for us to access in a different task.
-#     if task.host.get("mpls_full_mesh"):
-#         # print(task.host['mpls_full_mesh'])
-#         task.host["loopbacks"] = loopbacks
-#         # print(task.host["loopbacks"])
 
 
 def render_configs(task):
